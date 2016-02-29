94287f26c28311eecfb21a3581526ed25c2567eb
Courtney Nguyen <courtneythuy@gmail.com>
2016-02-28 18:42:51 -0700

    stuff

--- www/js/main.js
+++ www/js/main.js
@@ -1,16 +1,49 @@
 var main = (function(){
 	var todoStorage = null;
+	var todoList = [];
+
 	var start = function(){
-		todoStorage = localStorage;
-		console.log('starting');
+		todoStorage = window.localStorage;
+		if(!todoStorage.getItem("tasks")){
+			todoStorage.setItem("tasks", JSON.stringify([]));
+		}
+		updateTodoList();
 	};
-	var saveTodo = function(task){
-		var userId = 1;
-		todoStorage[userId]
+
+	var updateTodoList = function(){
+		todoList = readTodoList();
+		var listElement = document.getElementById('todo-list');
+		var htmlTodoList = todoList.map(function(task, index){
+			return '<li><input id="checkbox-'+ index +'" type="checkbox"><label for="checkbox-'+ index +'">'+ task +'</label></li>';
+		});
+		listElement.innerHTML = htmlTodoList.join('');
 	};
+
+	var saveTodo = function(e){
+		var taskElement = document.getElementById('task-value');
+		saveTaskToLocalStorage(taskElement.value);
+		taskElement.value = "";
+		e.preventDefault();
+		updateTodoList();
+	};
+
+	var saveTaskToLocalStorage = function(data){
+	    var tasks = [];
+	    tasks = JSON.parse(todoStorage.getItem('tasks'));
+	    tasks.push(data);
+	    todoStorage.setItem('tasks', JSON.stringify(tasks));
+	};
+
+	var readTodoList = function(){
+		return JSON.parse(todoStorage.getItem('tasks'));
+	};
+
 	return {
 		'start': start,
-		'saveTodo': saveTodo
+		'saveTodo': saveTodo,
+		'getTodoList': todoList
 	}
 
 })();
+
+document.getElementById('create-task').addEventListener('click', main.saveTodo);
